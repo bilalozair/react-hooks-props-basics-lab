c8101174227e03e5f5dd89cc75a9b30e955b0e05
bilalozair <bo569@nyu.edu>
2022-12-26 23:48:22 -0500

    commit tree c5ad2f6664c0b35b8e045eaa8022da40508501b7

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+
+  function bioExists(bio) {
+    if (bio == "" || bio == null){
+      return null
+    } else {
+      return <p>{bio}</p>
+    }
+
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioExists(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github = "https://github.com/liza" linkedin = "https://www.linkedin.com/in/liza/"/>
     </div>
   );
 }
